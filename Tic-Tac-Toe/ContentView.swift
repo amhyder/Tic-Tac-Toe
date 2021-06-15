f641e91731f4b8f7d344e80735bc256d70b22550
Austin Hyder <amhyder5@gmail.com>
2021-06-15 10:37:48 -0400

    Initial Commit

---
+++ Tic-Tac-Toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic-Tac-Toe
+//
+//  Created by Austin Hyder on 6/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Austin Hyder on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
