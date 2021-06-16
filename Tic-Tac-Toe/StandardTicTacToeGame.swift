1a65bdbed25089dfd94dfe10e44df9286c8e80e7
Austin Hyder <amhyder5@gmail.com>
2021-06-15 22:40:01 -0400

    Add model-view for game

--- Tic-Tac-Toe/StandardTicTacToeGame.swift
+++ Tic-Tac-Toe/StandardTicTacToeGame.swift
@@ -2,7 +2,32 @@
 //  StandardTicTacToeGame.swift
 //  Tic-Tac-Toe
 //
+//  Represents the model-view for the Tic-Tac-Toe game
+//
 //  Created by Austin Hyder on 6/15/21.
 //
 
-import Foundation
+import SwiftUI
+
+class StandardGame: ObservableObject {
+    
+    static var xPiece = "❌"
+    static var oPiece = "⭕️"
+    static var blankPiece = ""
+    
+    static func createTicTacToeGame() -> TicTacToeGame<String> {
+        TicTacToeGame<String>(xPiece: self.xPiece, oPiece: self.oPiece, blankPiece: self.blankPiece)
+    }
+    
+    @Published private var model: TicTacToeGame<String> = createTicTacToeGame()
+    
+    var board: Array<TicTacToeGame<String>.GameCell> {
+        return model.board
+    }
+    
+    //MARK: - Intent(s)
+    
+    func choose(_ cell: TicTacToeGame<String>.GameCell) {
+        model.placePiece(chosenCell: cell)
+    }
+}
